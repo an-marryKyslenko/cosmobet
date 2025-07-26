5b7c217cbd72ac65299ec85021b9db37dfa2c4d2
an-marryKyslenko <olih.a@icloud.com>
2025-07-26 17:08:45 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,25 @@
-const track = document.querySelector('.slider-track');
-let currentIndex = 0;
-const cardsPerView = 3;
-const cardCount = track.children.length;
+const authBlock = document.getElementById('authorized');
+const noAuhtBlock = document.getElementById('no-authorized');
+let isAuth = false;
 
-function slideNext() {
-	currentIndex += cardsPerView;
-
-	if (currentIndex >= cardCount / 2) {
-		currentIndex = 0;
-	}
-
-	const offset = currentIndex * (100 / cardsPerView);
-	track.style.transform = `translateX(-${offset}%)`;
+if(isAuth) {
+	noAuhtBlock.style.display = 'none';
+	authBlock.style.display = 'flex';
+} else {
+	noAuhtBlock.style.display = 'flex';
+	authBlock.style.display = 'none';
 }
 
-setInterval(slideNext, 3000);
+let timeoutId;
+
+window.addEventListener('load', () => {
+	const preloader = document.getElementById('preloader');
+	const app = document.getElementById('app');
+
+	clearTimeout(timeoutId);
+	timeoutId = setTimeout(() => {
+		preloader.style.display = 'none';
+		app.style.display = 'flex';
+	}, 3000)
+
+});
