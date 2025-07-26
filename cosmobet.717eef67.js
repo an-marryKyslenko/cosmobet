503d027b0f4ef6497a680173942c0569338097ea
an-marryKyslenko <olih.a@icloud.com>
2025-07-26 17:15:05 +0300

    Updates

---
+++ cosmobet.717eef67.js
@@ -0,0 +1,2 @@
+let e;const t=document.getElementById("authorized");document.getElementById("no-authorized").style.display="flex",t.style.display="none",window.addEventListener("load",()=>{let t=document.getElementById("preloader"),d=document.getElementById("app");clearTimeout(e),e=setTimeout(()=>{t.style.display="none",d.style.display="flex"},3e3)});
+//# sourceMappingURL=cosmobet.717eef67.js.map
